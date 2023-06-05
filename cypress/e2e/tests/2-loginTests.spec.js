6be81def439b094da191cd3de28277b3c6c184ff
Elena Dyagileva <edyagileva96@gmail.com>
2023-06-05 14:31:12 +0800

    forgot password tests

--- cypress/e2e/tests/2-loginTests.spec.js
+++ cypress/e2e/tests/2-loginTests.spec.js
@@ -30,7 +30,7 @@ describe("register tests", () => {
     );
   });
 
-  it.only("verifies invalid login - empty email and password fields", () => {
+  it("verifies invalid login - empty email and password fields", () => {
     cy.get(".btn-primary").click();
     cy.get(".alert-dismissible").should(
       "contain",
@@ -39,7 +39,7 @@ describe("register tests", () => {
   });
 
   it("verifies invalid login - email is not registered", () => {
-    cy.get("#input-email").type("hell1599@gmail.com");
+    cy.get("#input-email").type("hello@gmail.com");
     cy.get("#input-password").type("KateM123");
     cy.get(".btn-primary").click();
     cy.get(".alert-dismissible").should(
@@ -49,7 +49,7 @@ describe("register tests", () => {
   });
 
   it("verifies invalid login - email or password is wrong", () => {
-    cy.get("#input-email").type("hell1599@gmail.com");
+    cy.get("#input-email").type("hellthere1599o@gmail.com");
     cy.get("#input-password").type("zzzz1");
     cy.get(".btn-primary").click();
     cy.get(".alert-dismissible").should(
@@ -66,5 +66,34 @@ describe("register tests", () => {
       "contain",
       "Warning: No match for E-Mail Address and/or Password."
     );
+  });
+
+  it.only("verifies Forgot password functionality - with registered email", () => {
+    cy.get(".btn-default").click();
+    cy.get("#input-email").type("hellothere1599@gmail.com");
+    cy.get(".btn-primary").click({ force: true });
+    cy.get(".alert-success")
+      .should("be.visible")
+      .and(
+        "contain",
+        "An email with a confirmation link has been sent your email address."
+      );
+  });
+
+  it("verifies Forgot password functionality - not registered email", () => {
+    cy.get(".btn-default").click();
+    cy.get("#input-email").type("test@test.com");
+    cy.get(".btn-primary").click({ force: true });
+
+    cy.get(".alert-dismissible").should(
+      "contain",
+      "Warning: The E-Mail Address was not found in our records, please try again!"
+    );
+  });
+
+  it("verifies correct link - return back from Forgot password page to Login page ", () => {
+    cy.get(".btn-default").click(); //Forgotten password button
+    cy.get(".btn-default").click(); // back button
+    cy.url().should("contain", "user-login/");
   });
 });
