e76e2a62de32eb56a156912084fdf8441ae8b7b5
Elena Dyagileva <edyagileva96@gmail.com>
2023-06-13 14:52:24 +0800

    add to cart tests

--- cypress/e2e/tests/4-addToCartTests.spec.js
+++ cypress/e2e/tests/4-addToCartTests.spec.js
@@ -11,12 +11,26 @@ describe("Account tests", () => {
     cy.visit("https://eliteembroidery.co.uk/my-account/");
   });
 
-  it("verifies that user was logged in", () => {
+  it("verifies that item can be added to the cart", () => {
     cy.contains("T-Shirts & Vests").click();
-    cy.get(".product").each((product) => {
-      product.click();
-      cy.contains("Select a colour").click();
-      cy.get(".product-colours").find("ul").find("li").eq(0);
-    });
+    cy.get(".product").eq(0).click();
+    cy.get(".product-colours-control").click();
+    cy.contains("Dark Orange").click();
+    cy.get("#option-8334450-32485252").clear().type(3);
+    cy.contains("S").click(); // click outside input box
+    cy.get("#tm_add_to_cart").click();
+    cy.get(".swal2-actions").should("be.visible");
+  });
+
+  it("verifies that item with several sizes can be added to the cart", () => {
+    cy.contains("T-Shirts & Vests").click();
+    cy.get(".product").eq(0).click();
+    cy.get(".product-colours-control").click();
+    cy.contains("Melange Grey").click();
+    cy.get("#option-8334450-32485252").clear().type(3);
+    cy.get("#option-8334450-32485254").clear().type(2);
+    cy.contains("S").click(); // click outside input box
+    cy.get("#tm_add_to_cart").click();
+    cy.get(".swal2-actions").should("be.visible");
   });
 });
